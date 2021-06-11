90bd660ffb83a3cbb805221ffa7b7604efbb935e
EC2 Default User <ec2-user@ip-172-31-11-252.us-east-2.compute.internal>
2021-06-11 01:01:27 +0000

    Put and get functionalities done

--- dynamo_api/src/user.py
+++ dynamo_api/src/user.py
@@ -1,7 +1,6 @@
 import json
 import boto3
 import os
-from boto3.dynamodb.conditions import Key, Attr
 
 users_table = os.environ['USERS_TABLE']
 
@@ -10,28 +9,44 @@ table = dynamodb.Table(users_table)
 
 def getUser(event, context):
     print(json.dumps({"running": True}))
+    print(json.dumps(event))
+    
+    path = event["path"] # "/user/123"
+    array_path = path.split("/") # ["", "user", "123"]
+    user_id = array_path[-1]
     
     response = table.get_item(
         Key={
-            'pk': 'user_',
+            'pk': user_id,
             'sk': 'age'
         }
     )
     item = response['Item']
     print(item)
-
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps(item)
     }
-
+    
 def putUser(event, context):
     print(json.dumps({"running": True}))
+    print(json.dumps(event))
+    
+    path = event["path"] # "/user/123"
+    array_path = path.split("/") # ["", "user", "123"]
+    user_id = array_path[-1]
+    
+    body = event["body"] #"{\n\t\"name\": \"Jack\",\n\t\"last_name\": \"Click\",\n\t\"age\": 21\n}"
+    body_object = json.loads(body)
+    
     
     table.put_item(
         Item={
-            'pk': 'user_',
-            'sk': 'age'
+            'pk': user_id,
+            'sk': 'age',
+            'name': body_object['name'],
+            'last_name': body_object['last_name'],
+            'age': body_object['age']
         }
     )
     
